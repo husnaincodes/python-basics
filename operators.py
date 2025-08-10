fa3b34db9335ddeaba28554153a031879a6a5c6a
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 16:43:31 +0500

    OPERATORS

---
+++ operators.py
@@ -0,0 +1,33 @@
+
+#ARITHMETIC OPERATOR
+a = 23
+b = 56
+c = a+b
+print(c)
+
+
+
+#assignment operators
+a = 45-34
+print(a)
+b = 45
+b *= 5
+print(b)
+ #comparsion opertors
+d = 5<6
+print(d)
+e = 56== 67
+print(e)
+
+
+
+
+#logical opterators
+f = True or False
+print(f)
+g = True and False
+print(g)
+ 
+
+
+ 
