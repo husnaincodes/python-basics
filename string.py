0e180d30c0b20ce4b1d61e1ffd97bd0fd24c988c
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 17:05:09 +0500

    STRING

---
+++ string.py
@@ -0,0 +1,24 @@
+
+name4 = "husnain"
+name2 = '''gaara'''
+name3 = 'goku'
+print(name4,name2,name3)
+
+
+
+name = 'HUSNAIN'
+t = len(name)
+
+print(t)
+
+
+name1 = 'HUSNAIN'
+t = name1 [0:5]
+
+print(t)
+
+
+
+name5 = "vegeta"
+d = name5[:-4]
+print(d)
