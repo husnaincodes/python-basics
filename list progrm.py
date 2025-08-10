558d9964bd55a215e22ca0498f204ee3d332e4dd
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 17:24:00 +0500

    LIST PROGROM

---
+++ list progrm.py
@@ -0,0 +1,28 @@
+
+list = []
+
+f1 = input("ENTER FURIT NAME :")
+list.append(f1)
+
+f2 = input("ENTER FURIT NAME :")
+list.append(f2)
+
+f3 = input("ENTER FURIT NAME :")
+list.append(f3)
+
+f4 = input("ENTER FURIT NAME :")
+list.append(f4)
+
+f5 = input("ENTER FURIT NAME :")
+list.append(f5)
+
+f6 = input("ENTER FURIT NAME :")
+list.append(f6)
+
+f7 = input("ENTER FURIT NAME :")
+list.append(f7)
+
+
+
+print(list)
+
