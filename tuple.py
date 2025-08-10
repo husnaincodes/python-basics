42aa0abcdf77dbfb340ea78d905b55a330737703
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 17:27:55 +0500

    TUPLE

---
+++ tuple.py
@@ -0,0 +1,33 @@
+
+my_tuple = (1,3,4,56,7,8,9,00,56,67)
+print(my_tuple)
+no = my_tuple.count(56)
+print(no)
+
+
+a = my_tuple*3
+print(a)
+
+b = my_tuple.index(67)
+print(b)
+
+
+
+fruits = ('apple', 'banana', 'cherry')
+for fruit in fruits:
+    print(fruit)
+
+
+my_list = [1 ,3 ,4 ,5 ,7 ,"husnain", "gaara"]
+c = tuple(my_list)
+print(c)
+
+my_name = ("husnain ",6 ,7 ,8 ,0,4,False,True,45.6)
+print(0 in my_name)
+print(69 in my_name)
+
+
+
+
+h = input()
+print(55 in h)
