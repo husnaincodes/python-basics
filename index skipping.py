9f48c072e2fae727495d296d331ab4d588735ea9
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 17:02:48 +0500

    INDEX SKIPPKING

---
+++ index skipping.py
@@ -0,0 +1,25 @@
+
+a = "abcdefghijklmnopqrstuvwxyz"
+# d = a[0:25]
+# print(d)
+
+
+# f = a [0:9:2]
+# print(f)
+
+
+# g = a [0:26:3]
+# print(g)
+
+# h = a [3:8:4]
+# print(h)
+
+
+# j = a [0:25 :5]
+# print(j)
+
+
+k = a [1 :9:4]
+
+print(k)
+
