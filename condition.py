7149c5e041b36c1342a8a7ead6205fb69c63d8a4
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 16:37:07 +0500

    CONDITION

---
+++ condition.py
@@ -0,0 +1,12 @@
+e = int(input("ENTER YOUR 1 NUMBER :"))
+f = int(input("ENTER YOUR 2 NUMBER  :"))
+print("a is greater than b is ", e<f)
+
+
+
+c = int ( input(" ENTER YOUR NUMBER :"))
+d = int (input(" ENTER YOUR NUMBER :"))
+print(" the average of two number is ", (c+d)/2)
+
+a = int ( input(" ENTER YOUR NUMBER :"))
+print("THE SQUARE OF THIS NUMBER IS ",a*a)
