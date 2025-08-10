a3f4188ada64baf091ab8f1b93c649d807e92bba
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 16:41:28 +0500

    INPUT VARIABLES

---
+++ input.py
@@ -0,0 +1,17 @@
+
+a = int(input("ENTER THE FIRST NUMBER :"  ))
+
+b = int(input("ENTER YOUR SECOND NUMBER : "))
+
+
+print("a number is ",a)
+print("b number is ", b)
+print("division of a and b is ", a/b)
+
+# a = float(input("ENTER THE FIRST NUMBER :"  ))
+# b = float(input("ENTER THE second NUMBER :"  ))
+
+
+# print("a number is ", a)
+# print("b number is ",b)
+# print("division of a and b is ",a/b)
