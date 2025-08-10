c51deb9967d90cde9528e912c12e834ff22835a8
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 17:10:54 +0500

    STRING FUNCTION

---
+++ string function.py
@@ -0,0 +1,21 @@
+
+name = input("ENTER YOUR NAME :")
+print(len(name))
+
+print(name.endswith("in"))
+
+print(name.startswith("hus"))
+
+print(name.capitalize())
+
+print(name.lower()) 
+
+print(name.upper())
+
+print(name.title())
+
+
+a = "my name is hunain tayab i completed my inter from government college and my matric from private school"
+print(a.capitalize())
+
+print(a.index("scho "))
