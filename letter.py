1c96f8615bcdb2ddb10bd249f6ada0e84eb9abcd
Husnain <Husnain.tayab@hotmail.com>
2025-08-10 17:00:35 +0500

    LETTER

---
+++ letter.py
@@ -0,0 +1,13 @@
+
+name = input("ENTER YOUR NAME PLEASE :")
+print (f"GOOD AFTERNOON, {name} ")
+
+
+letter = """
+DEAR <|NAME|>,
+            YOUR ARE SELECTED!
+<|DATE|>
+
+"""
+print(letter.replace( "<|NAME|>","HUSNAIN").replace("<|DATE|>","5 aug 2025"))
+
