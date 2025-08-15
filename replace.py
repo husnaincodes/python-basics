f6f66266503c7f79880acfc20c20f2440e4617a2
Husnain <Husnain.tayab@hotmail.com>
2025-08-15 16:36:22 +0500

    Replace

--- replace.py
+++ replace.py
@@ -1,12 +1,12 @@
 
-a = """Gravity is the force that pulls objects toward each other.
+phyiscs = """Gravity is the force that pulls objects toward each other.
 On Earth, it gives us weight and keeps us   grounded.
 The greater an object’s mass,the stronger its gravitational pull.
 That’s why the Sun’s gravity holds planets in orbit.
 Even though gravity seems weak compared to other forces, 
 it shapes the universe on the largest scales."""
 
-print(a.find("scales"))
-print(a.replace("stronger","heavy"))
+print(phyiscs.find("scales"))
+print(phyiscs.replace("stronger","heavy"))
 
-print(a)
+print(phyiscs)
