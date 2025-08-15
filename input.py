305801ef36378d944097b1a867605c90b96b9929
Husnain <Husnain.tayab@hotmail.com>
2025-08-15 16:21:43 +0500

    Input Number

--- input.py
+++ input.py
@@ -1,17 +1,17 @@
 
-a = int(input("ENTER THE FIRST NUMBER :"  ))
+number = int(input("ENTER THE FIRST NUMBER :"  ))
 
-b = int(input("ENTER YOUR SECOND NUMBER : "))
-
-
-print("a number is ",a)
-print("b number is ", b)
-print("division of a and b is ", a/b)
-
-# a = float(input("ENTER THE FIRST NUMBER :"  ))
-# b = float(input("ENTER THE second NUMBER :"  ))
+input_number = int(input("ENTER YOUR SECOND NUMBER : "))
 
 
-# print("a number is ", a)
-# print("b number is ",b)
-# print("division of a and b is ",a/b)
+print("a number is ",number)
+print("b number is ", input_number)
+print("division of a and b is ", number/input_number)
+
+# input_number = float(input("ENTER THE FIRST NUMBER :"  ))
+# number2 = float(input("ENTER THE second NUMBER :"  ))
+
+
+# print("a number is ", input_number)
+# print("b number is ",number2)
+# print("division of a and b is ",input_number/number2)
