70a2be24063d23ff6aa911884fc9d0213e574141
Husnain <Husnain.tayab@hotmail.com>
2025-09-30 15:24:07 +0500

    Cube/Square

--- cube.py
+++ cube.py
@@ -2,6 +2,6 @@
 number = int(input('ENTER THE NUMBER: '))
 
 if number%2==0:
-    print(f"The sqaure of this {number} number is : {number*number}")
+    print(f"The square of this {number} number is : {number*number}")
 else:
     print(f"The cube of this {number} number is : {number*number*number}")
