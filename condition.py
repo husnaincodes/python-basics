e551e1b246917ff06d6d22de894080828eb6853a
Husnain <Husnain.tayab@hotmail.com>
2025-09-30 15:18:12 +0500

    Condition operator

--- condition.py
+++ condition.py
@@ -1,12 +1,15 @@
 number = int(input("ENTER YOUR 1 NUMBER :"))
+
 number2 = int(input("ENTER YOUR 2 NUMBER  :"))
+
 print("a is greater than b is ", number<number2)
 
-
-
 number = int ( input(" ENTER YOUR NUMBER :"))
-number2 = int (input(" ENTER YOUR NUMBER :"))
+
+number2 = int (input("ENTER YOUR NUMBER :"))
+
 print(" the average of two number is ", (number+number2)/2)
 
-square = int ( input(" ENTER YOUR NUMBER :"))
+square = int ( input("ENTER YOUR NUMBER :"))
+
 print("THE SQUARE OF THIS NUMBER IS ",square*square)
