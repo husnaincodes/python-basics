a762f21e8c0d35cfdb43df929f598ff6015c4ea5
Husnain <Husnain.tayab@hotmail.com>
2025-10-01 20:37:50 +0500

    Variables

--- variable.py
+++ variable.py
@@ -1,9 +1,9 @@
 
-# variable  =1 #int data type 
-# variable2= "harry" # char data type 
-# variable3 = 45.6 #float data type
+variable  =1 #int data type 
+variable2= "harry" # char data type 
+variable3 = 45.6 #float data type
 # variable4 = True/False #boolean data type
-# variable5 = None #none data type 
+variable5 = None #none data type 
 
 my_variable = 3
 variable1 = 10
