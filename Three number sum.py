195646f15c3a96833392a2b051d7bbbbf24fdac3
Husnain <Husnain.tayab@hotmail.com>
2025-10-04 16:52:16 +0500

    Three numbers sum

---
+++ Three number sum.py
@@ -0,0 +1,8 @@
+
+number =int(input("Enter the First number : ")) 
+
+number2 = int(input("Enter the second number : ")) 
+
+number3= int(input("Enter the thrid  number : ")) 
+
+print(f"The sum of these three number is : {number+number2+number3}")
