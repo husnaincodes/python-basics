b74f391bfe264aea7bd247bd5df6ce286e1f05e9
Husnain <Husnain.tayab@hotmail.com>
2025-10-09 00:48:51 +0500

    Six digit Sum

---
+++ SIX digit sum.py
@@ -0,0 +1,11 @@
+
+number = int(input("Enter a Six digit number: "))
+d1 = number//100000
+d2 = number//10000%10
+d3 = number//1000%10
+d4 = number//100%10
+d5 = number//10%10
+d6 = number%10
+
+sum_digit = d1+d2+d3+d4+d5+d6
+print(sum_digit)
