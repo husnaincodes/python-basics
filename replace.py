3ab54da795c429da132e046399af6b51beb417be
Husnain <Husnain.tayab@hotmail.com>
2025-10-08 21:35:48 +0500

    Replace word

--- replace.py
+++ replace.py
@@ -4,7 +4,7 @@ On Earth, it gives us weight and keeps us   grounded.
 The greater an object’s mass,the stronger its gravitational pull.
 That’s why the Sun’s gravity holds planets in orbit.
 Even though gravity seems weak compared to other forces, 
-it shapes the universe on the largest scales."""
+"""
 
 print(phyiscs.find("scales"))
 print(phyiscs.replace("stronger","heavy"))
