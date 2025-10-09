8ff5782d0c5e95b47257b6d668a7ddc7786d125b
Husnain <Husnain.tayab@hotmail.com>
2025-10-09 17:13:44 +0500

    Largest number

---
+++ Largestr number.py
@@ -0,0 +1,11 @@
+number =int(input("Enter the First number : ")) 
+number2 = int(input("Enter the Second number : ")) 
+number3= int(input("Enter the Thrid  number : ")) 
+
+if number>number2>number3:
+    print("Number 1 is Largest number")
+
+elif number2>number3>number:
+    print("Number 2 is Largest number")
+else:
+    print("Number 3 is Largest number")
