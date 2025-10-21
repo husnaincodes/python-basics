d2641afb69ad792d95b188370e2db3edd1867cee
Husnain <Husnain.tayab@hotmail.com>
2025-10-21 23:16:09 +0500

    Sets&Tuple

---
+++ program15.py
@@ -0,0 +1,8 @@
+fruits = ["apple","banana","apple","mango","banana"]
+my_set = set(fruits)
+my_tuple = tuple(my_set)
+print(len(my_tuple))
+if "orange" in my_tuple:
+    print("Exist")
+else:
+    print("Not Exist")
