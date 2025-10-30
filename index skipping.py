fadabc0d0424166c9c3cacfffa0a255591035332
Husnain <Husnain.tayab@hotmail.com>
2025-10-30 20:57:31 +0500

    Index skipping

--- index skipping.py
+++ index skipping.py
@@ -2,22 +2,16 @@
 my_variable = "abcdefghijklmnopqrstuvwxyz"
 index = my_variable[0:25]
 print(my_variable)
-
-
 index2 = my_variable [0:9:2]
 print(index2)
 
-
 skip = my_variable [0:26:3]
 print(skip)
-
 skip2 = my_variable [3:8:4]
 print(skip)
 
-
 skip3 = my_variable [0:25 :5]
 print(skip3)
-
 
 slice1 = my_variable [1 :9:4]
 
