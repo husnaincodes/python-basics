e272b8c5a3c6c6add5e4d17c675d3d4f1aefc303
Husnain <Husnain.tayab@hotmail.com>
2025-11-14 23:32:15 +0500

    Escape sequence

--- escape sequence.py
+++ escape sequence.py
@@ -1,3 +1,3 @@
-line  = "\"Gravity\" is not just a force—it’s the curvature of spacetime \t itself. \n 'Massive' objects like stars and planets warp the fabric of reality,\n bending the paths of light  "
+line  = "\"Gravity\" is not just a force—its the curvature of spacetime \t itself. \n 'Massive' objects like stars and planets warp the fabric of reality,\n bending the paths of light  "
 
 print(line)
