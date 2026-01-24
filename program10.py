819a67066c623f9a33e31082dd61873c9712005b
Husnain <Husnain.tayab@hotmail.com>
2026-01-24 22:11:42 +0500

    List Program

--- program10.py
+++ program10.py
@@ -1,11 +1,14 @@
 nums = [10, 20, 30, 20, 40, 10]
 my_nums = set(nums)
 nums1 = tuple(my_nums)
+
 print(type(nums))
 print(type(my_nums))
 print(type(nums1))
+
 print(len(nums1))
 print(nums1[0],nums1[-1])
+
 if 30 in nums1:
     print("Yes")
 else:
