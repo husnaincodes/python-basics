92509228f7bea548b61a82d866415f73e0b1d353
Husnain <Husnain.tayab@hotmail.com>
2026-03-20 22:24:17 +0500

    Python program

--- program15.py
+++ program15.py
@@ -1,10 +1,12 @@
-fruits = ["apple","banana","apple","mango","banana"]
+fruits = ["apple","banana","apple","mango","banana","peach"]
 my_set = set(fruits)
 
 my_tuple = tuple(my_set)
 print(len(my_tuple))
 
 if "orange" in my_tuple:
+    print("Exist")
+elif "apple" in my_tuple:
     print("Exist")
 else:
     print("Not Exist")
